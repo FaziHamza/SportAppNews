c6d8e0a38bed22606d7cdc380533a0c1e449f3cb
Aman Saeed <amantatla312@gmail.com>
2024-02-19 17:03:57 +0500

    some fixings

--- src/app/shared/layout/shared-header/shared-header.component.ts
+++ src/app/shared/layout/shared-header/shared-header.component.ts
@@ -1,13 +1,14 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { SharedSidebarComponent } from "../shared-sidebar/shared-sidebar.component";
 import { ThemeService } from '../../../theme-service.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-shared-header',
     standalone: true,
     templateUrl: './shared-header.component.html',
     styleUrl: './shared-header.component.scss',
-    imports: [SharedSidebarComponent]
+    imports: [RouterModule]
 })
 export class SharedHeaderComponent {
 
